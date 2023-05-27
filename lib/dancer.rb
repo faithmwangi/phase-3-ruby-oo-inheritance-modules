410c20b82df656eed6ece2f757b0cb6df9d6c4a9
Faith Mwangi <faith.mwangi01@student.moringaschool.com>
2023-05-27 20:02:24 +0300

    Update dance module and dancer/kid classes

--- lib/dancer.rb
+++ lib/dancer.rb
@@ -1,11 +1,9 @@
-require_relative './dance_module'
-require_relative './meta_dancing_module'
+require_relative 'dance_module.rb'
+require_relative 'meta_dancing_module.rb'
 
 class Dancer
   include Dance
   extend MetaDancing
-  extend FancyDance::ClassMethods
-  include FancyDance::InstanceMethods
 
   attr_accessor :name
 
